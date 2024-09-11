e471c9e8481308e207799f908d7183e2d37b39e7
williancsmelo <melo.williancs@gmail.com>
2024-09-11 20:04:52 -0300

    removendo node modules

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,48 @@
-import Glpk, { Options } from 'glpk.js'
+import Glpk from 'glpk.js'
 
-const glpk = Glpk()
+const main = async () => {
+  const glpk = Glpk()
 
-const res = glpk.solve(
-  {
-    name: 'LP',
-    objective: {
-      direction: glpk.GLP_MAX,
-      name: 'obj',
-      vars: [
-        { name: 'x1', coef: 0.6 },
-        { name: 'x2', coef: 0.5 }
+  return
+  const res = glpk.solve(
+    {
+      name: 'LP',
+      objective: {
+        direction: glpk.GLP_MAX,
+        name: 'obj',
+        vars: [
+          { name: 'x1', coef: 0.6 },
+          { name: 'x2', coef: 0.5 }
+        ]
+      },
+      subjectTo: [
+        {
+          name: 'cons1',
+          vars: [
+            { name: 'x1', coef: 1.0 },
+            { name: 'x2', coef: 2.0 }
+          ],
+          bnds: { type: glpk.GLP_UP, ub: 1.0, lb: 0.0 }
+        },
+        {
+          name: 'cons2',
+          vars: [
+            { name: 'x1', coef: 3.0 },
+            { name: 'x2', coef: 1.0 }
+          ],
+          bnds: { type: glpk.GLP_UP, ub: 2.0, lb: 0.0 }
+        }
       ]
     },
-    subjectTo: [
-      {
-        name: 'cons1',
-        vars: [
-          { name: 'x1', coef: 1.0 },
-          { name: 'x2', coef: 2.0 }
-        ],
-        bnds: { type: glpk.GLP_UP, ub: 1.0, lb: 0.0 }
-      },
-      {
-        name: 'cons2',
-        vars: [
-          { name: 'x1', coef: 3.0 },
-          { name: 'x2', coef: 1.0 }
-        ],
-        bnds: { type: glpk.GLP_UP, ub: 2.0, lb: 0.0 }
+    {
+      msglev: glpk.GLP_MSG_ALL,
+      presol: true,
+      cb: {
+        call: progress => console.log(progress),
+        each: 1
       }
-    ]
-  },
-  {
-    msglev: glpk.GLP_MSG_ALL,
-    presol: true,
-    cb: {
-      call: progress => console.log(progress),
-      each: 1
     }
-  }
-)
+  )
+}
+
+main().then(res => res !== undefined && console.log(res))
